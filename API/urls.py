2609ab614f250007051de1dd3a04737e03a26c05
Nai-Ekta-Satish-kumar <ektan@zignuts.com>
2025-02-21 15:12:40 +0530

    Class Based Views

--- API/urls.py
+++ API/urls.py
@@ -2,11 +2,20 @@ from django.urls import path,include
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns = [
-    path("list/",views.list_movies),
-    path("list/<int:pk>/",views.movie),
-    path("stream/",views.platform_detail),
-    path("stream/<int:pk>",views.platform),
 
+# Functional Based View 
+# urlpatterns = [
+#     path("list/",views.list_movies),
+#     path("list/<int:pk>/",views.movie),
+#     path("stream/",views.platform_detail),
+#     path("stream/<int:pk>",views.platform),
+# ]
+
+# Class Based View
+urlpatterns = [
+    path('platforms/',views.StreamPlatformList.as_view()),
+    path('platforms/<int:pk>/',views.StreamPlatformDetails.as_view())
 ]
+
+
 urlpatterns = format_suffix_patterns(urlpatterns)
--- API/views.py
+++ API/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from .models import WatchList
 from .serializers import *
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.views import APIView
 
 # Create your views here.
 @api_view(['GET','POST'])
@@ -30,13 +33,14 @@ def movie(request,pk,format = None):
     serializer = WatchlistSerializer(movie)
     return Response(serializer.data)
 
-@api_view(['GET','POST'])
-def platform_detail(request,format = None):
-    if request.method == 'GET':
-       stream = StreamPlatform.objects.all()
-       serialized = StreamPlatformSerializer(stream,many= True)
-       return Response(serialized.data,status=status.HTTP_200_OK)
-    elif request.method == 'POST':
+
+class StreamPlatformList(APIView):
+   def get(self,request,format = None):
+      stream = StreamPlatform.objects.all()
+      serialized = StreamPlatformSerializer(stream,many= True)
+      return Response(serialized.data,status=status.HTTP_200_OK)
+   
+   def post(self,request,format = None):
        data = request.data
        serialized = StreamPlatformSerializer(data = data)
        if serialized.is_valid():
@@ -45,29 +49,79 @@ def platform_detail(request,format = None):
        else:
           return Response(serialized.data,status=status.HTTP_406_NOT_ACCEPTABLE)
 
-@api_view(['GET','PUT','DELETE'])
-def platform(request,pk,format = None):
 
-    try:
-       platform = StreamPlatform.objects.get(pk = pk)
-    except Exception:
-       return Response(Exception,status=status.HTTP_404_NOT_FOUND)
+class StreamPlatformDetails(APIView):
+   
+   def get_object(self,pk):
+      try:
+         platform = StreamPlatform.objects.get(pk = pk)
+         return platform
+      except:
+         return Response(status=status.HTTP_404_NOT_FOUND)
+      
 
-    if request.method == 'GET':
-       serialized = StreamPlatformSerializer(platform)
-       return Response(serialized.data,status=status.HTTP_200_OK)
-    
-    elif request.method == 'PUT':
+   def get(self,request,pk,format = None):
+     platform = self.get_object(pk)
+     serialized = StreamPlatformSerializer(platform)
+     return Response(serialized.data,status=status.HTTP_200_OK)
+   
+   def put(self,request,pk,format = None):
         newdata = request.data  
-        serialized = StreamPlatformSerializer(platform,data = newdata)
+        serialized = StreamPlatformSerializer(self.get_object(pk),data = newdata)
         if serialized.is_valid():
             serialized.save()
             return Response(serialized.data,status=status.HTTP_200_OK)
         else:
            return Response(serialized.data,status=status.HTTP_304_NOT_MODIFIED)
+   
+   def delete(self,request,pk,format = None):
+      self.get_object(pk).delete()
+      return Response(status=status.HTTP_200_OK)
+
+
+
+
+
+
+# --------------------------------------------------------------------------------
+# @api_view(['GET','POST'])
+# def platform_detail(request,format = None):
+#     if request.method == 'GET':
+#        stream = StreamPlatform.objects.all()
+#        serialized = StreamPlatformSerializer(stream,many= True)
+#        return Response(serialized.data,status=status.HTTP_200_OK)
+#     elif request.method == 'POST':
+#        data = request.data
+#        serialized = StreamPlatformSerializer(data = data)
+#        if serialized.is_valid():
+#           serialized.save()
+#           return Response(serialized.data,status=status.HTTP_201_CREATED)
+#        else:
+#           return Response(serialized.data,status=status.HTTP_406_NOT_ACCEPTABLE)
+# --------------------------------------------------------------------------------
+# @api_view(['GET','PUT','DELETE'])
+# def platform(request,pk,format = None):
+
+#     try:
+#        platform = StreamPlatform.objects.get(pk = pk)
+#     except Exception:
+#        return Response(Exception,status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == 'GET':
+#        serialized = StreamPlatformSerializer(platform)
+#        return Response(serialized.data,status=status.HTTP_200_OK)
+    
+#     elif request.method == 'PUT':
+#         newdata = request.data  
+#         serialized = StreamPlatformSerializer(platform,data = newdata)
+#         if serialized.is_valid():
+#             serialized.save()
+#             return Response(serialized.data,status=status.HTTP_200_OK)
+#         else:
+#            return Response(serialized.data,status=status.HTTP_304_NOT_MODIFIED)
         
-    elif request.method == 'DELETE':
-       platform.delete()
-       return Response(status=status.HTTP_200_OK)
-        
+#     elif request.method == 'DELETE':
+#        platform.delete()
+#        return Response(status=status.HTTP_200_OK)
+# --------------------------------------------------------------------------------
     
